ddc61abb124ec1d2c4ec19fb2245cf2a22c9ef77
Alex Kautz <akautz@u.rochester.edu>
2020-10-17 14:06:51 -0400

    Added basic colision

--- Player.gd
+++ Player.gd
@@ -1,12 +1,14 @@
-extends Node2D
+extends KinematicBody2D
+
+# https://docs.godotengine.org/en/stable/classes/class_node2d.html
+# https://docs.godotengine.org/en/stable/tutorials/physics/using_kinematic_body_2d.html
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
-var xVelocity = 20
-var yVelocity = 20
+const velocity = Vector2(100, 100)
 
 
 # Called when the node enters the scene tree for the first time.
@@ -16,5 +18,11 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position[0] = position[0] + ( xVelocity * delta )
-	position[1] = position[1] + ( yVelocity * delta )
+	var collision_data = move_and_collide(velocity * delta)
+
+	if collision_data:
+		print("I hit ", collision_data.collider.name)
+		velocity[1] = - velocity[1]
+	
+	#position[0] = position[0] + ( xVelocity * delta )
+	#position[1] = position[1] + ( yVelocity * delta )
