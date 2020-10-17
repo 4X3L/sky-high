f2807cefbcb86459ce366d218c1c1a174521b7fa
Alex Kautz <akautz@u.rochester.edu>
2020-10-17 14:45:52 -0400

    Added walls and colision checking

--- Player.gd
+++ Player.gd
@@ -2,27 +2,28 @@ extends KinematicBody2D
 
 # https://docs.godotengine.org/en/stable/classes/class_node2d.html
 # https://docs.godotengine.org/en/stable/tutorials/physics/using_kinematic_body_2d.html
+# https://docs.godotengine.org/en/stable/getting_started/step_by_step/scripting_continued.html#groups
 
+# Our velocity
+const velocity = Vector2(0, 0)
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-const velocity = Vector2(100, 100)
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	velocity[0] = 200
+	velocity[1] = 200
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	# Update the position of the player and check for collisions 
 	var collision_data = move_and_collide(velocity * delta)
 
 	if collision_data:
-		print("I hit ", collision_data.collider.name)
-		velocity[1] = - velocity[1]
-	
-	#position[0] = position[0] + ( xVelocity * delta )
-	#position[1] = position[1] + ( yVelocity * delta )
+		var groups = collision_data.collider.get_groups()
+		
+		# Did we hit a platform or a wall?
+		if "Platforms" in groups:
+			print("Platform found!")
+			# TODO add platform code
+		elif "Walls" in groups:
+			print("Wall found!")
+			velocity[0] = - velocity[0]
