21cbf5588e5794d894cffe6ea53ae2e727ee8133
Alex Benishek <alex@alexbenishek.com>
2020-10-18 18:17:34 -0400

    simplified spawn logic

--- PlatGen.gd
+++ PlatGen.gd
@@ -1,38 +1,29 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-export var yPlayer : float = 0.0
 var platformScn = load("res://Platform.tscn")
 var platformList = []
-var minBounds = 0
-var maxBounds = 0
-var spawnDist = 200000
-var spawnPerRange = 3000
+var platcount = 1000
 var rng = RandomNumberGenerator.new()
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-func spawnInRange(ub, lb):
-	for _i in range(0,spawnPerRange):
+	var bottom = 5120
+	var top = -16384
+	var left = 0
+	var right = 1024
+	for _i in range(0,platcount):
 		var newPlat = platformScn.instance()
 		var viewWidth = get_viewport().size.x
-		newPlat.position[0] = rng.randf_range(0, viewWidth)
-		newPlat.position[1] = rng.randf_range(lb, ub)
+		newPlat.position[0] = rng.randf_range(left, right)
+		newPlat.position[1] = rng.randf_range(top, bottom)
 		newPlat.add_to_group("Platforms")
 		platformList.append(newPlat)
 		add_child_below_node(get_tree().get_root(), newPlat)
+	
+
+func spawnInRange(ub, lb):
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	while abs(minBounds - yPlayer) < spawnDist:
-		spawnInRange(minBounds, minBounds-spawnDist)
-		minBounds -= spawnDist
-	while abs(maxBounds - yPlayer) < spawnDist:
-		spawnInRange(maxBounds + spawnDist, maxBounds)
-		maxBounds += spawnDist
-	
+	pass
 	
