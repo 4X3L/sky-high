eb8bcee93e956fa28bac6ceaa72303d4b63af5e5
Alex Kautz <akautz@u.rochester.edu>
2020-10-18 18:55:16 -0400

    Fixed burger win

--- Burger.gd
+++ Burger.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
 func _on_Player_body_entered(body):
-	get_tree().change_scene("res://Winning.tscn")
+	if "Players" in body.get_groups():
+		get_tree().change_scene("res://Winning.tscn")
 
 func _ready():
 	self.connect("body_entered", self, "_on_Player_body_entered")
