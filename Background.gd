a27b298e6c1e5213d542b32b4786bd3e1b26407f
Alex Benishek <axel@compucito.fios-router.home>
2020-10-17 20:05:11 -0400

    Merge changes from main

--- Background.gd
+++ Background.gd
@@ -6,13 +6,21 @@ extends Node2D
 # var b = "text"
 
 # var xVelocity = 20
-var yVelocity = 200
+
+var velocity = 20
+
+onready var node = get_node("/root/Node2D/Player")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
+var oldY = 0.0
+var hasDiff = false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position[1] = position[1] + (yVelocity * delta)
+	if hasDiff: 
+		#Should not need the "yDirection" part for the Player node
+		position[1] -= (oldY - node.position[1]) * velocity * delta
+	hasDiff = true
+	oldY = node.position[1]
