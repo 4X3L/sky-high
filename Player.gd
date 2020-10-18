022f18f08a91fff6c6439e0ab785527be3fe8fe5
Alex Kautz <akautz@u.rochester.edu>
2020-10-18 15:48:15 -0400

    Restricted to one jump

--- Player.gd
+++ Player.gd
@@ -17,32 +17,52 @@ export var JUMP_POWER : float = 2
 export var JUMP_POWER_FIXED : float = 1000
 export var FIXED : bool = false
 
+export var TOTAL_JUMPS = 1
+var curent_jumps
 # var my_sprite : Sprite = null
 onready var my_sprite = $Sprite
+
+export var JUMP_COLOR : Color = Color(.7, .7, 1)
+export var NO_JUMP_COLOR : Color = Color(1, 1, 1)
 
 func _ready():
 	velocity[0] = 0
 	velocity[1] = 0
-	# my_sprite = get_node("Sprite") # TODO async?
+	curent_jumps = 0
+	_update_sprite()
+
+
+func _update_sprite():
+	# Direction
+	my_sprite.set_flip_h( velocity[0] < 0 )
+	# Color
+	if curent_jumps > 0:
+		my_sprite.modulate = JUMP_COLOR
+	else:
+		my_sprite.modulate = NO_JUMP_COLOR
+
+func _jump():
+	var direction = get_viewport().get_mouse_position() - get_global_transform_with_canvas().origin 
+	if FIXED:
+		velocity = direction.normalized() * JUMP_POWER_FIXED
+	else:
+		velocity = direction * JUMP_POWER
 
 # Called on input
 func _input(event):
 	if event.is_action_pressed("game_click"):
-		#velocity[1] = -800
+		if curent_jumps > 0:
+			_jump()
+			curent_jumps -= 1
+	_update_sprite()
 		
-		velocity = get_viewport().get_mouse_position() - get_global_transform_with_canvas().origin 
-		
-		if FIXED:
-			velocity = velocity.normalized() * JUMP_POWER_FIXED
-		else:
-			velocity = velocity * JUMP_POWER
-		
-	
+
 
 func _gravity():
 	velocity[1] = velocity[1] + GRAVITY
 
 func _on_plaform_land(delta, collision_data):
+	curent_jumps = TOTAL_JUMPS
 	var oldv = velocity
 	velocity = velocity.slide(collision_data.normal)
 	if collision_data.travel[1] < bounceClamp:
@@ -60,9 +80,6 @@ func _process(delta):
 	# Update the position of the player and check for collisions 
 	var collision_data = move_and_collide(velocity * delta)
 
-	# Update sprite
-	my_sprite.set_flip_h( velocity[0] < 0 )
-
 	if collision_data:
 		var groups = collision_data.collider.get_groups()
 		
@@ -74,6 +91,8 @@ func _process(delta):
 			_gravity()
 		else:
 			print("Colidded with: ", collision_data.collider.name)
+		
+		_update_sprite()
 	else:
 		_gravity()
 
